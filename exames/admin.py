805069ba64fb6db32aa504d0f523054ca625f60f
Fabricio Damasceno <68025089+FabricioFerreiraDamasceno@users.noreply.github.com>
2023-10-06 18:14:27 -0300

    Primeiro commit

---
+++ exames/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+
+# Register your models here.
+from django.contrib import admin
+from .models import TiposExames, PedidosExames, SolicitacaoExame,AcessoMedico
+
+admin.site.register(TiposExames)
+admin.site.register(PedidosExames)
+admin.site.register(SolicitacaoExame)
+admin.site.register(AcessoMedico)
